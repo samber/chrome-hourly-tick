e7243a94b67f5aa0b33016c8b490dc41923e2186
Samuel BERTHE <contact@samuel-berthe.fr>
2015-09-17 15:06:29 +0700

    increased sample volume and encoded in wav

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.alarms.create("one-hour-tick", {
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
     if (Math.floor(Date.now() / 1000 / 60) % 60 == 0) {
-	var audio = new Audio("./tick.mp3");
+	var audio = new Audio("./tick.wav");
 	audio.play();
     }
 });
